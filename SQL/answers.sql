8e6a625d1bf1b1532c44ff940f4d7652cb2d880b
afosam <afosam2013@gmail.com>
2025-08-25 11:40:33 +0100

    Create answers.sql

---
+++ SQL/answers.sql
@@ -0,0 +1,11 @@
+-- answers.sql
+-- Assignment: Basic SQL Database Commands
+-- Author: [Afolabi SAmuel]
+-- Date: [25/08/2025]
+
+/* Question 1: Create salesDB */
+CREATE DATABASE salesDB;
+
+/* Question 2: Drop demo database */
+CREATE DATABASE salesDB;
+DROP DATABASE IF EXISTS demo;
